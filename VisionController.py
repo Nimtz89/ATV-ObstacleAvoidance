09a7ab3c2bae267c6752613296549ba34bad1202
Anco van den Berg <anco.vandenberg@winvision.nl>
2023-07-21 22:03:26 +0200

    Added vision controller for azure computer vision

---
+++ VisionController.py
@@ -0,0 +1,62 @@
+import os
+import cv2
+from dotenv import load_dotenv
+
+import azure.ai.vision as sdk
+
+load_dotenv()
+service_options = sdk.VisionServiceOptions(os.environ["VISION_ENDPOINT"],
+                                           os.environ["VISION_KEY"])
+camera = cv2.VideoCapture(0)
+if not camera.isOpened():
+ print("Cannot open camera")
+ exit()
+
+camera.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+try:
+    ret, image = camera.read()
+    cv2.imwrite("picture_captured.png", image)
+           
+    vision_source = sdk.VisionSource(
+        filename="picture_captured.png")
+    analysis_options = sdk.ImageAnalysisOptions()
+    analysis_options.features = (
+        sdk.ImageAnalysisFeature.CAPTION |
+        sdk.ImageAnalysisFeature.TEXT
+    )
+    analysis_options.language = "en"
+
+    analysis_options.gender_neutral_caption = True
+
+    image_analyzer = sdk.ImageAnalyzer(service_options, vision_source, analysis_options)
+
+    result = image_analyzer.analyze()
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:
+
+        if result.caption is not None:
+            print(" Caption:")
+            print("   '{}', Confidence {:.4f}".format(result.caption.content, result.caption.confidence))
+
+        if result.text is not None:
+            print(" Text:")
+            for line in result.text.lines:
+                points_string = "{" + ", ".join([str(int(point)) for point in line.bounding_polygon]) + "}"
+                print("   Line: '{}', Bounding polygon {}".format(line.content, points_string))
+                
+                if (line.content.lower() == "turn right"):
+                    print("Turn right")
+
+                if (line.content.lower() == "turn left"):
+                    print("Turn left")
+                
+    else:
+
+        error_details = sdk.ImageAnalysisErrorDetails.from_result(result)
+        print(" Analysis failed.")
+        print("   Error reason: {}".format(error_details.reason))
+        print("   Error code: {}".format(error_details.error_code))
+        print("   Error message: {}".format(error_details.message))
+except KeyboardInterrupt:
+    camera.release()
